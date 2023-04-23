c6401223dcbc286b494625d954b3f9b502f52e6e
samu7861 <sfrancog7861@gmail.com>
2023-04-23 16:13:07 -0500

    styles

---
+++ script.js
@@ -0,0 +1,4 @@
+//llamado de elementos del htmñ
+const stopwatch = document.getElementById('stopwatch');
+const playPauseButton = document.getElementById('play-pause');
+const secondsSphere = document.getElementById('seconds-sphere');
